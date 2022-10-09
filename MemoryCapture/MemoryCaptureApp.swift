b7d0ba0d32ca8d5603bf47d65a85885ad7963920
Suguru Tokuda <suguru.tokuda@gmail.com>
2022-10-09 11:41:05 -0500

    Initial Commit

---
+++ MemoryCapture/MemoryCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryCaptureApp.swift
+//  MemoryCapture
+//
+//  Created by Suguru on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
